220b206a90f556e45f27096be90ed36ad7d822cd
Oliver Assis <oliverinvest08@gmail.com>
2025-07-15 22:54:42 -0300

    comecando o repositorio de exercicios

---
+++ ex01/index.js
@@ -0,0 +1,52 @@
+// // 👉 Cria uma variável idade e outra chamada temCarteira (boolean).
+// // Usa operador lógico (&&) pra verificar se a pessoa tem 18 anos ou mais e tem carteira.
+// // Mostra “Pode dirigir!” ou “Não pode dirigir!”.
+
+// let idade = 18;
+// let temCarteira = true;
+
+// if (idade >= 18 && temCarteira == true) {
+//     console.log("Você pode dirigir!")
+// } else {
+//     console.log("Você não pode dirigir.")
+// }
+
+
+// // 👉 Cria duas variáveis numero1 e numero2.
+// // Usa operadores de comparação (>=, <=, !==) pra mostrar no console:
+// // Se numero1 é maior ou igual a numero2
+// // Se numero1 é diferente de numero2
+
+// let numero1 = "n";
+// let numero2 = 8;
+
+// if (numero1 >= numero2) {
+//     console.log(`O número ${numero1} é maior que o número ${numero2}`)
+// } else if (numero1 <= numero2) {
+//     console.log(`O número ${numero1} é menor que o número ${numero2}`)
+// } else if (numero1 !== numero2) {
+//     console.log(`O número ${numero1} é diferente do número ${numero2}`)
+// }
+
+// 
+
+// // ☑️ 4) Repetição - while (nível médio)
+// // 👉 Cria uma variável contador = 1.
+// // Usa while pra contar até 5 (inclusive), mostrando cada número no console.
+
+// let contador = 1;
+
+// while (contador <= 5) {
+//     console.log(contador);
+//     contador ++;
+// }
+
+// ☑️ 5) Repetição - do...while (nível médio+)
+// 👉 Pede ao usuário um número maior que 0.
+// Se o número for menor ou igual a 0, pede novamente, até digitar corretamente.
+// Usa do...while!
+
+
+
+
+
