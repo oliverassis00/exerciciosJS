b66b2cce84c6da534142ddb665c0488b2d25b1d4
Oliver Assis <oliverinvest08@gmail.com>
2025-07-28 21:09:41 -0300

    atualicacao exercicios

---
+++ exercicios/index2.js
@@ -0,0 +1,21 @@
+// // 1. Peça pro usuário digitar a idade dele. Mostre no console:
+
+// const prompt = require('prompt-sync')();
+
+// let digiteIdade = parseInt(prompt("Digite sua idade: "));
+
+// if (digiteIdade >= 18) {
+//     console.log("Você é maior de idade.")
+// } else {
+//     console.log("Você é menor de idade.")
+// }
+
+// // 2. Peça pro usuário digitar um número inteiro positivo N. Mostre no console os números de 1 até N.
+
+// const prompt = require('prompt-sync')();
+
+// let pegarInteiro = parseInt(prompt("Digite um número inteiro: "));
+
+// for (let i = 1; i <= pegarInteiro; i ++) {
+//     console.log(i)
+// }
