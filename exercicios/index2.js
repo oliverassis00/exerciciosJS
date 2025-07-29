65099ef29a16f807cbfe78765712a282041958ef
Oliver Assis <oliverinvest08@gmail.com>
2025-07-28 22:23:23 -0300

    Atualizacao Geral

--- exercicios/index2.js
+++ exercicios/index2.js
@@ -19,3 +19,38 @@
 // for (let i = 1; i <= pegarInteiro; i ++) {
 //     console.log(i)
 // }
+
+// // 3. Peça pro usuário digitar uma nota de 0 a 10.
+
+// const prompt = require('prompt-sync')();
+
+// let alunoNota = parseInt(prompt("Digite uma Nota: "));
+
+// if (alunoNota < 5) {
+//     console.log("Reprovado");
+// } else if (alunoNota >= 5 && alunoNota < 7) {
+//     console.log("Recuperação");
+// } else {
+//     console.log("Aprovado");
+// }
+
+// // 5. Crie uma função chamada calcularMedia que receba 3 números como parâmetros. A função deve calcular e retornar a média.
+
+// const calcularMedia = (numero1, numero2, numero3) => (numero1 + numero2 + numero3) / 3;
+
+// console.log(calcularMedia(10,10,10))
+
+// 6. Peça pro usuário digitar um número inteiro positivo N. Mostre no console o fatorial desse número.
+
+const prompt = require('prompt-sync')();
+
+let numero = parseInt(prompt("Digite um número inteiro positivo: "));
+let resultado = 1;
+
+for (let i = numero; i >= 1; i--) {
+    resultado *= i;
+}
+
+console.log(`O fatorial de ${numero} é ${resultado}`);
+
+
